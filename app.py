e91c2a952c34f819a3b43eb4e794b323511ef3fd
Aetrix27 <davedogg323@gmail.com>
2021-03-25 20:21:11 -0700

    Reverted

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 """Import and run app."""
-from easyhealth_app import app
+from easyhealth_app import create_app
+
+app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+#"""Import and run app."""
+#from easyhealth_app import app
+
+#if __name__ == "__main__":
+#    app.run(debug=True)
+
